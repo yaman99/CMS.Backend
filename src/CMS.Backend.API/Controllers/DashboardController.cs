814f8bc2dbead03bfec7a4771f76b2d56baa1def
yaman99 <yaman.sawandev@gmail.com>
2024-06-01 19:07:18 +0300

    add dashboard

---
+++ src/CMS.Backend.API/Controllers/DashboardController.cs
@@ -0,0 +1,33 @@
+﻿using CMS.Backend.Appilication.Features.UsersManagement.Commands;
+using CMS.Backend.Application.Common.Models;
+using CMS.Backend.Application.Features.CommunityManagement;
+using CMS.Backend.Application.Features.CoursesManagement.Commands;
+using CMS.Backend.Application.Features.CoursesManagement.Queries;
+using CMS.Backend.Application.Features.Dashboard;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class DashboardController : BaseController
+    {
+        [HttpGet("get-instructor-dashboard")]
+        public async Task<IActionResult> GetInstructorDashboard()
+            => Ok(await Bus.ExecuteAsync<GetInstructorDashboardQuery, Result>(new GetInstructorDashboardQuery()));
+
+        [HttpGet("get-admin-dashboard")]
+        public async Task<IActionResult> GetAdminDashboard()
+            => Ok(await Bus.ExecuteAsync<GetAdminDashboardQuery, Result>(new GetAdminDashboardQuery()));
+
+        [HttpGet("get-student-dashboard")]
+        public async Task<IActionResult> GetStudentDashboard()
+            => Ok(await Bus.ExecuteAsync<GetStudentDashboardQuery, Result>(new GetStudentDashboardQuery()));
+
+
+
+
+    }
+}
---
+++ src/CMS.Backend.Application/Features/Dashboard/DashboardHandler.cs
@@ -0,0 +1,79 @@
+﻿using CMS.Backend.Appilication.Repository;
+using CMS.Backend.Application.Common.Interfaces;
+using CMS.Backend.Application.Common.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Backend.Application.Features.Dashboard
+{
+    public class DashboardHandler :
+        IRequestHandler<GetInstructorDashboardQuery, Result>,
+        IRequestHandler<GetAdminDashboardQuery, Result>,
+        IRequestHandler<GetStudentDashboardQuery, Result>
+    {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly ICommunityRepoistory _communityRepoistory;
+        private readonly IUserRepository _userRepository;
+        private readonly ICoursesRepoistory _coursesRepository;
+
+        public DashboardHandler(ICurrentUserService currentUserService, ICommunityRepoistory communityRepoistory, IUserRepository userRepository, ICoursesRepoistory coursesRepository)
+        {
+            _currentUserService = currentUserService;
+            _communityRepoistory = communityRepoistory;
+            _userRepository = userRepository;
+            _coursesRepository = coursesRepository;
+        }
+
+        public async Task<Result> Handle(GetInstructorDashboardQuery request, CancellationToken cancellationToken)
+        {
+            var instructorId = Guid.Parse(_currentUserService.UserId!);
+            var communities = await _communityRepoistory.GetinstructorCommunities(instructorId);
+            var courses = await _coursesRepository.GetinstructorCourses(instructorId);
+
+            var result = new
+            {
+                TotalStudent = courses.Select(x => x.EnrolledStudents).Count(),
+                TotalCourses = courses.Count(),
+                TotalCommunities = communities.Count(),
+            };
+
+            return Result.Success(result);
+
+        }
+
+        public async Task<Result> Handle(GetAdminDashboardQuery request, CancellationToken cancellationToken)
+        {
+            var communities = await _communityRepoistory.GetAllCommunities();
+            var courses = await _coursesRepository.GetAllCourses();
+            var users = await _userRepository.GetAllAsync();
+
+            var result = new
+            {
+                TotalStudents = users.Count(x => x.UserType == "student"),
+                TotalInstructors = users.Count(x => x.UserType == "instructor"),
+                TotalCourses = courses.Count(),
+                TotalCommunities = communities.Count(),
+            };
+            return Result.Success(result);
+
+        }
+
+        public async Task<Result> Handle(GetStudentDashboardQuery request, CancellationToken cancellationToken)
+        {
+            var studentId = Guid.Parse(_currentUserService.UserId!);
+            var communities = await _communityRepoistory.GetStudentCommunities(studentId);
+            var courses = await _coursesRepository.GetStudentCourses(studentId);
+
+            var result = new
+            {
+                TotalCourses = courses.Count(),
+                TotalCommunities = communities.Count(),
+            };
+            return Result.Success(result);
+        }
+    }
+}
